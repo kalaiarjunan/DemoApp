e87a00c221e04e63be54dcaef7e500be32af13a4
KrithikaV11 <krithi.ramya@gmail.com>
2014-06-09 17:12:27 +0530

    modified

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,6 +1,6 @@
 class CommentController < ApplicationController
  def create
-#Added for git testing
+#Added for git testing for git
         @post = Post.find(params[:post_id])
         @comment = @post.comment.create(params[:comment])
         redirect_to post_path(@post)
